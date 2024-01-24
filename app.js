107edca9ac27471627995fd6979fc68f7e18c915
jmanwel <new.juang@gmail.com>
2024-01-24 16:37:45 -0300

    [ADD] adding files

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const cluster = process.env.MONGO_CLUSTER;
 
 const app = express();
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extend: true}))
 const mongodb = `mongodb+srv://${user}:${pass}@${cluster}/${db}?retryWrites=true&w=majority`;
 
 mongoose.connect(mongodb, {useNewUrlParser: true})
@@ -19,39 +20,50 @@ mongoose.connect(mongodb, {useNewUrlParser: true})
     })
     .catch((err)=> console.log(err))
 
-app.get("/create_item", (req, res)=>{
-    const item = new Item({
-        name: "Laptop",
-        price: 2000
-    });
-    item.save().then(r=> res.send(r))
-})
-
-app.get("/get_items", (req, res)=>{
-    
-    Item.find()
-        .then(r=> res.send(r))
-        .catch(e => console.log(e))
-});
-
-app.get("/get_item", (req, res)=>{
-    
-    Item.findById("65b13ec123be23ea43a71490")
-        .then(r=> res.send(r))
-        .catch(e => console.log(e))
-});
-
 app.get("/", (req, res)=>{
-    const items = [
-        {name: "item1", price: 100},
-        {name: "item2", price: 200},
-        {name: "item3", price: 300}
-    ]
-    res.render("index", {items});
+    res.redirect("get_items");
 })
+
+app.get("/get_items", (req, res)=>{    
+    Item.find()
+        .then(r=>  
+            res.render("index", {items: r})
+        )
+        .catch(e => console.log(e))
+});
 
 app.get("/add_item", (req, res)=>{
     res.render("add_item");
+})
+
+app.post("/items", (req, res)=>{
+    console.log(req.body);
+    const item = Item(req.body);
+    item.save()
+        .then(()=>{
+            res.redirect("/")
+        })
+        .catch((e)=> console.log(e))
+})
+
+app.get("/items/:id", (req, res)=>{
+    console.log(req.params);
+    const id = req.params.id;
+    Item.findById(id)
+        .then((r)=>{
+            console.log(r)
+            res.render("item_detail", {item:r});
+        })
+})
+
+app.delete("/items/:id", (req, res)=>{
+    console.log(req.params);
+    const id = req.params.id;
+    Item.findByIdAndDelete(id)
+        .then((r)=>{
+            console.log("item deleted");
+            res.redirect("/")
+        })
 })
 
 app.use((req, res)=>{
