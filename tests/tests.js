912434384a104d503b0ab0e3acc32c049b59c659
jmanwel <new.juang@gmail.com>
2024-06-04 16:22:37 -0300

    [MOD] test.js

--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,15 @@
 var assert = require('assert');
-assert(1==1);
-  
+
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+   
+describe('Basic', function () {
+    it('Simple 1 = 1', function () {
+    assert.equal(1, 1);
+    });
+});
